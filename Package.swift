c125c982fe166e2d85435c1dbb5d1a2cda6ab8a7
Christopher Prince <chris@SpasticMuffin.biz>
2016-12-30 12:40:42 -0700

    Release 0.0.1: Added tag, and comment

--- Package.swift
+++ Package.swift
@@ -1,5 +1,7 @@
 import PackageDescription
 
+// Trying to figure out how to make this dependency test-only. See http://stackoverflow.com/questions/41401753/test-only-dependencies-when-using-the-swift-package-manger
+
 let package = Package(
     name: "SMServerLib",
     dependencies: [
